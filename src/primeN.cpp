ea90492d4dd03ff0f1b98d3a9a78fbb83cc39837
Your Name <you@example.com>
2022-01-13 16:52:06 -0500

    heh

--- src/primeN.cpp
+++ src/primeN.cpp
@@ -1,110 +1,47 @@
-// C++ program Miller-Rabin primality test
-#include <bits/stdc++.h>
-using namespace std;
- 
-// Utility function to do modular exponentiation.
-// It returns (x^y) % p
-int power(int x, unsigned int y, int p)
-{
-    int res = 1;      // Initialize result
-    x = x % p;  // Update x if it is more than or
-                // equal to p
-    while (y > 0)
-    {
-        // If y is odd, multiply x with result
-        if (y & 1)
-            res = (res*x) % p;
- 
-        // y must be even now
-        y = y>>1; // y = y/2
-        x = (x*x) % p;
+#include <iostream>
+#include <cmath>
+#include <sodium.h>
+
+using std::cout;
+
+ int modulo(int a, int b, int n){
+    long long x=1, y=a; 
+    while (b > 0) {
+        if (b%2 == 1) {
+            x = (x*y) % n;
+        }
+        y = (y*y) % n;
+        b /= 2;
     }
-    return res;
+    return x % n;
 }
- 
-// This function is called for all k trials. It returns
-// false if n is composite and returns true if n is
-// probably prime.
-// d is an odd number such that  d*2<sup>r</sup> = n-1
-// for some r >= 1
-bool miillerTest(int d, int n)
-{
-    // Pick a random number in [2..n-2]
-    // Corner cases make sure that n > 4
-    int a = 2 + rand() % (n - 4);
- 
-    // Compute a^d % n
-    int x = power(a, d, n);
- 
-    if (x == 1  || x == n-1)
-       return true;
- 
-    // Keep squaring x while one of the following doesn't
-    // happen
-    // (i)   d does not reach n-1
-    // (ii)  (x^2) % n is not 1
-    // (iii) (x^2) % n is not n-1
-    while (d != n-1)
-    {
-        x = (x * x) % n;
-        d *= 2;
- 
-        if (x == 1)      return false;
-        if (x == n-1)    return true;
+
+
+bool singleTest(int n, int a) {
+    int exp = n - 1;
+    
+    while(!(exp & 1)) {
+        exp >> 1;
     }
- 
-    // Return composite
+
+    if (modulo(a, exp, n) == 1 ) {
+        return true;
+    }
+    while(exp < n -1) {
+        if (modulo(a, exp, n) == n -1) {
+            return true;
+        }
+        exp << 1;
+    }
     return false;
 }
- 
-// It returns false if n is composite and returns true if n
-// is probably prime.  k is an input parameter that determines
-// accuracy level. Higher value of k indicates more accuracy.
-bool isPrime(int n, int k)
-{
-    // Corner cases
-    if (n <= 1 || n == 4)  return false;
-    if (n <= 3) return true;
- 
-    // Find r such that n = 2^d * r + 1 for some r >= 1
-    int d = n - 1;
-    while (d % 2 == 0)
-        d /= 2;
- 
-    // Iterate given nber of 'k' times
-    for (int i = 0; i < k; i++)
-         if (!miillerTest(d, n))
-              return false;
- 
-    return true;
-}
- 
-// Driver program
-int findPrime(int nth, int k) {
-    int count = 0;
 
-    for (int n = 1; n < nth; n++) {
-       if (isPrime(n, k)) {
-           count++;
-       }
-    }
 
-    return count;
- 
-}
 
-int main(int argc, char *argv[]) {
-    if(argc < 2) {
-        cout << "You must supply a value\n";
-    }
-    const int n = atoi(argv[1]);
 
-<<<<<<< Updated upstream
-    cout << findPrime(n, 80) << std::endl;
-=======
-    const int max = atoi(argv[1]);
-    int count = findPrime(2, max);
+int main() {
+    int die = randombytes_uniform(100);
+    cout << die;
 
-    cout << count << "\n";
     return 0;
->>>>>>> Stashed changes
+}
