3d0084f542cbfe77ed0be879e7b0b4026f3b9618
wsl machine <anoond@cock.li>
2022-09-25 20:43:49 -0400

    added files

--- src/prime_miller.cc
+++ src/prime_miller.cc
@@ -1,29 +1,66 @@
 #include <iostream>
 #include <thread>
 #include <future>
+#include <sodium.h>
 using std::thread;
 using std::cout;
 using std::async;
 using std::future;
 
-
-int findPrime(int min, int max) {  // Prime Finding Function
-    int count, num, c = 0;
-    for(int i=min; i<=max; i++)
-        {
-        num = i;
-        count = 0;  
-        for(int x = 1; x <= num; x++) {
-            if(num%x == 0) {
-                count++;
-            }
+ int modulo(int a, int b, int n){
+    long long x=1, y=a; 
+    while (b > 0) {
+        if (b%2 == 1) {
+            x = (x*y) % n;
         }
-        if(count == 2) {  
-            c++;
-        }  
+        y = (y*y) % n;
+        b /= 2;
+    }
+    return x % n;
+}
+
+
+bool singleTest(int n, int a) {
+    int exp = n - 1;
+    
+    while(!(exp & 1)) {
+        exp >>= 1;
     }
 
-    return c;
+    if (modulo(a, exp, n) == 1 ) {
+        return true;
+    }
+    while(exp < n -1) {
+        if (modulo(a, exp, n) == n -1) {
+            return true;
+        }
+        exp <<= 1;
+    }
+    return false;
+}
+
+bool miller_rabin(int n, int k = 2) {
+    int a = 0;
+    for(int i = 0; i<k; i++) {
+        a = randombytes_uniform(n-1)+2;
+        if(!(singleTest(n, a))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int primeFinder(int min, int max) {
+    int count = 0;
+
+    for(int i=min; i<=max; i++) {
+        bool value = miller_rabin(i);
+        if(value) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 
@@ -31,15 +68,15 @@ int main(int argc, char *argv[]) {
     if(argc < 2) {
         cout << "You must supply a value\n";
     }
+    const int MAX = atoi(argv[1]);
 
     int primes = 0;
 
     const auto processor_count = std::thread::hardware_concurrency();
-    const int MAX = atoi(argv[1]);
     const int MINIMUM_VALUE = processor_count*2;
     const int MIN = 2;
     if((MAX-MIN)<MINIMUM_VALUE) { // Checks if the range you specified is greater than the min value you can calculate using your cores
-        primes = findPrime(MIN, MAX);
+        primes = primeFinder(MIN, MAX);
     }
     else {
         int arr[processor_count][2] = {{}};
@@ -56,7 +93,7 @@ int main(int argc, char *argv[]) {
 
         future<int> values[processor_count] = {};
         for(int i=0; i<processor_count; i++) { // Calls the findPrime function and calculates
-            values[i] = async(findPrime, arr[i][0], arr[i][1]);
+            values[i] = async(primeFinder, arr[i][0], arr[i][1]);
         }
         for(int i=0; i<processor_count; i++) { // Adds up results, this needed to be in a separate loop or else the performace would be the same as primeN.cpp
             primes+=values[i].get();
--- src/prime_miller.rs
+++ src/prime_miller.rs
@@ -1,9 +1,8 @@
 use rand::Rng;
 
 fn modulo(a: i128, mut b: i128, n: i128) -> i128 {
-    let mut x:i128 = 1;
-    let mut y:i128 = a;
-
+    let mut x: i128 = 1;
+    let mut y: i128=a; 
     while b > 0 {
         if b%2 == 1 {
             x = (x*y) % n;
@@ -18,7 +17,7 @@ fn modulo(a: i128, mut b: i128, n: i128) -> i128 {
 fn single_test(n: i128, a: i128) -> bool {
     let mut exp: i128 = n - 1;
     
-    while (exp & 1) != 0  {
+    while !(exp & 1) != 0 {
         exp >>= 1;
     }
 
@@ -34,24 +33,24 @@ fn single_test(n: i128, a: i128) -> bool {
     return false;
 }
 
-fn miller_rabin(n: i128, test_runs: i128) -> bool {
-    let mut a = rand::thread_rng();
-    for _ in 0..test_runs {
-        let a: i128 = a.gen_range(0..=n-1)+2; 
-        if !single_test(n, a) {
+fn miller_rabin(n: i128, k: i128) -> bool {
+    for _ in 0..k {
+        let mut a = rand::thread_rng();
+        let a: i128 = a.gen_range(0..n-1)+2;
+        if !(single_test(n, a)) {
             return false;
         }
     }
     return true;
 }
 
-pub fn find_prime(max: i128) -> i128 {
-    let mut count: i128 = 0;
+pub fn find_prime(min: i128, max: i128) -> i128 {
+    let mut count: i128= 0;
 
-    for i in 2..=max {
-        let value: bool = miller_rabin(i, 100);
+    for i in min..=max {
+        let value: bool = miller_rabin(i, 2);
         if value {
-            count+= 1;
+            count+=1;
         }
     }
 
